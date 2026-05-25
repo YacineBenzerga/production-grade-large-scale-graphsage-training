1f0943a91383d19e366934d7af56d819a9e5aed7
Yacine Benzerga <20939542+FalconFive55@users.noreply.github.com>
2026-05-24 22:49:57 -0400

    added graphsage architecture with skip connections, node own features and label correlation tensor

---
+++ src/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import SAGEConv
+
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int, num_layers: int, dropout: float):
+        super().__init__()
+        self.convs = torch.nn.ModuleList()
+        self.dropout = dropout
+        
+        if num_layers == 1:
+            self.convs.append(SAGEConv(in_channels, out_channels))
+        else:
+            self.convs.append(SAGEConv(in_channels, hidden_channels))
+            for _ in range(num_layers - 2):
+                self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+            self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
+        for i, conv in enumerate(self.convs[:-1]):
+            x = conv(x, edge_index)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index)
+        return x
